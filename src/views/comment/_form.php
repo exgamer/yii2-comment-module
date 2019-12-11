2d7f702192088c4314701d09c3597bf2fd498401
citizenzet <exgamer@live.ru>
2019-12-11 17:59:06 +0600

    fr

--- src/views/comment/_form.php
+++ src/views/comment/_form.php
@@ -31,46 +31,49 @@ use concepture\yii2logic\enum\StatusEnum;
         </div>
         <div class="card-body">
             <?php if (! isset($originModel) || (isset($originModel) && $originModel->isNewRecord)) : ?>
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <?= Html::activeHiddenInput($model, 'user_id') ?>
+                        <?= Html::activeLabel($model, 'user_id')?>
+                        <?= \yii\jui\AutoComplete::widget([
+                            'name' => 'name',
+                            'value' => $model->getUserName(),
+                            'options' => ['class' => 'form-control'],
+                            'clientOptions' => [
+                                'source' => \yii\helpers\Url::to(['/user/user/list']),
+                                'minLength'=>'2',
+                                'autoFill'=>true,
+                                'select' => new \yii\web\JsExpression("function( event, ui ) {
+                                            $('#commentform-user_id').val(ui.item.id);
+                                     }")]
+                        ]); ?>
+                        <?= Html::error($model, 'user_id', ['class' => 'text-danger form-text'])?>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <?= $form->field($model, 'parent_id')->textInput(['maxlength' => true]) ?>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <?= $form
+                            ->field($model, 'entity_type_id')
+                            ->dropDownList(Yii::$app->entityTypeService->catalog(), [
+                                'class' => 'form-control custom-select',
+                                'prompt' => ''
+                            ]);
+                        ?>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <?= $form->field($model, 'entity_id')->textInput(['maxlength' => true]) ?>
+                    </div>
+                </div>
+
+            <?php endif;?>
             <div class="row">
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <?= Html::activeHiddenInput($model, 'user_id') ?>
-                    <?= Html::activeLabel($model, 'user_id')?>
-                    <?= \yii\jui\AutoComplete::widget([
-                        'name' => 'name',
-                        'value' => $model->getUserName(),
-                        'options' => ['class' => 'form-control'],
-                        'clientOptions' => [
-                            'source' => \yii\helpers\Url::to(['/user/user/list']),
-                            'minLength'=>'2',
-                            'autoFill'=>true,
-                            'select' => new \yii\web\JsExpression("function( event, ui ) {
-                                        $('#commentform-user_id').val(ui.item.id);
-                                 }")]
-                    ]); ?>
-                    <?= Html::error($model, 'user_id', ['class' => 'text-danger form-text'])?>
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <?= $form->field($model, 'parent_id')->textInput(['maxlength' => true]) ?>
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <?= $form
-                        ->field($model, 'entity_type_id')
-                        ->dropDownList(Yii::$app->entityTypeService->catalog(), [
-                            'class' => 'form-control custom-select',
-                            'prompt' => ''
-                        ]);
-                    ?>
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <?= $form->field($model, 'entity_id')->textInput(['maxlength' => true]) ?>
-                </div>
-                <?php endif;?>
                 <div class="col-lg-6 col-md-6 col-sm-12">
                     <?= $form
                         ->field($model, 'status')
@@ -83,8 +86,8 @@ use concepture\yii2logic\enum\StatusEnum;
                 <div class="col-lg-12 col-md-12 col-sm-12">
                     <?= $form->field($model, 'content')->textarea(); ?>
                 </div>
-
             </div>
+
         </div>
         <div class="card-body text-right">
             <?=  Html::submitButton(
